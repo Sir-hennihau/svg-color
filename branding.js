e2c95a83a261216bd8b23e37f3ee566d6ef373a1
Hendrik Hausen <hendrik.hausen@mail.de>
2021-01-08 10:18:55 +0100

    initial commit

---
+++ branding.js
@@ -0,0 +1,11 @@
+const defaultBranding = {
+    primaryColor: '#00C39A',
+    secondaryColor: '#6194b0',
+    tertiaryColor: '#F5CA4F',
+};
+
+const customerBranding = {
+    primaryColor: '#F6F740',
+    secondaryColor: '#995D81',
+    tertiaryColor: '#EB8258',
+};
